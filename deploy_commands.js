dd4c8f4cbb3b42337ceffdc17fb653c0d9cea956
Ethan Lee <LeethalEthan1@gmail.com>
2021-08-28 17:17:57 -0500

    Add files via upload

--- deploy_commands.js
+++ deploy_commands.js
@@ -2,23 +2,10 @@
 // Made by: L33#7732 | Adopted from the DiscordJS Upgrade Guide
 // Made on: 8.28.2021
 
-// Step 1: Run 'npm install @discordjs/builders @discordjs/rest discord-api-types'
-// Step 2: Fill out the configuration variables below.
-// Step 3: Make commands in the commands object.
-// Step 4: Ensure that your bot has the 'applications.commands' scope authorized.
-// Step 5: Run the script with 'node ./deploy-commands.js'
-// Step 6: ???
-// Step 7: Now your server(s) have commands!
-
 const { Routes } = require('discord-api-types/v9');
 const { REST } = require('@discordjs/rest');
 const { SlashCommandBuilder } = require('@discordjs/builders');
-
-// Configuration
-const deployLocal = true;  // When true, commands will only deploy to guild specified below. (This is instant, as compared to global which takes up to an hour.)
-const token = '';          // Bot Token
-const clientID = '';       // Client ID
-const guildID = '';        // Guild ID (Only needed for local deployment)
+const { token, clientID, guildID, deploymentMode } = require('./config.json');
 
 // Commands
 const commands = [
